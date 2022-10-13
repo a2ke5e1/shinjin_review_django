174573643e3572120d4334f3cc8927b69dcfeddc
= <aakapurv@gmail.com>
2022-10-13 20:06:32 +0530

    Partially Added Twitter Embed,

--- web-app/custom_tiptap_extensions/Twitter/TwitterComponent.tsx
+++ web-app/custom_tiptap_extensions/Twitter/TwitterComponent.tsx
@@ -1,22 +1,21 @@
-import { NodeViewWrapper } from '@tiptap/react'
+import {NodeViewWrapper} from '@tiptap/react'
 import React from 'react'
+import {Tweet} from 'react-twitter-widgets'
 
 // @ts-ignore
 // eslint-disable-next-line react/display-name
 export default props => {
-  const increase = () => {
-    props.updateAttributes({
-      count: props.node.attrs.count + 1,
-    })
-  }
+    const increase = () => {
+        props.updateAttributes({
+            count: props.node.attrs.count + 1,
+        })
+    }
 
-  return (
-    <NodeViewWrapper className="react-component">
-      <div className="content">
-        <button onClick={increase}>
-          This button has been clicked {props.node.attrs.count} times.
-        </button>
-      </div>
-    </NodeViewWrapper>
-  )
+    return (
+        <NodeViewWrapper className="react-component">
+            <div className="content">
+                <Tweet tweetId={"1580292557145505793"} />
+            </div>
+        </NodeViewWrapper>
+    )
 }
