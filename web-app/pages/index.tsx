f34cb34e608c246d867c4e44035221184733c742
= <aakapurv@gmail.com>
2022-10-14 23:21:50 +0530

    Added SASS and Fixed Some padding

--- web-app/pages/index.tsx
+++ web-app/pages/index.tsx
@@ -1,7 +1,5 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Tiptap from "../component/Editor/Tiptap";
 
 const Home: NextPage = () => {
@@ -16,7 +14,7 @@ const Home: NextPage = () => {
             </Head>
 
             <main>
-                <Tiptap />
+                <Tiptap/>
             </main>
 
 
