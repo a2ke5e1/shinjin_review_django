e2e6018ad3289de8498d5f7e2473e910560cc5f5
Apurv Ajay Kumar <aakapurv@gmail.com>
2022-10-29 11:54:20 +0530

    Made Tweets and ImageViewer Draggable and made tweeter dark

--- web-app/custom_tiptap_extensions/ImageViewer/ImageViewerComponent.tsx
+++ web-app/custom_tiptap_extensions/ImageViewer/ImageViewerComponent.tsx
@@ -9,36 +9,38 @@ import {ImageProps} from "./ImageViewer";
 export default props => {
 
 
-    const artwork: Array<ImageProps> = props.node.attrs.src
+  const artwork: Array<ImageProps> = props.node.attrs.src
 
-    let imageListCol = 3;
+  let imageListCol = 3;
 
 
-    const isWidth900 = useMediaQuery('(max-width: 900px)');
-    const isWidth400 = useMediaQuery('(max-width: 400px)');
+  const isWidth900 = useMediaQuery('(max-width: 900px)');
+  const isWidth400 = useMediaQuery('(max-width: 400px)');
 
-    if (isWidth900) {
-        imageListCol = 2;
-    }
+  if (isWidth900) {
+    imageListCol = 2;
+  }
 
-    if (isWidth400) {
-        imageListCol = 1;
-    }
+  if (isWidth400) {
+    imageListCol = 1;
+  }
 
-    return (
-        <NodeViewWrapper draggable="true"  data-drag-handle="" className="image-viewer-embed">
-            <ImageList cols={imageListCol} sx={{ p: 0 }}>
-                {artwork.map((item) => (
-                    <ImageListItem key={item.alt}>
-                        <img
-                            src={`${item.url}`}
-                            srcSet={`${item.url}`}
-                            alt={item.alt}
-                            loading="lazy"
-                        />
-                    </ImageListItem>
-                ))}
-            </ImageList>
-        </NodeViewWrapper>
-    )
+  return (
+    <NodeViewWrapper draggable="true" data-drag-handle="" className="image-viewer-embed">
+      <ImageList cols={imageListCol} sx={{p: 0}}>
+        {
+          artwork.map((item) => (
+            <ImageListItem key={item.alt}>
+              <img
+                src={`${item.url}`}
+                srcSet={`${item.url}`}
+                alt={item.alt}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))
+        }
+      </ImageList>
+    </NodeViewWrapper>
+  )
 }
